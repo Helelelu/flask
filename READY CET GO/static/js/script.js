0202dc54909a59d5eb6390bee0f6a33f1a02bc74
Helelelu <haellarenz@gmail.com>
2024-02-10 09:17:07 +0800

    login and other href fix

--- READY CET GO/static/js/script.js
+++ READY CET GO/static/js/script.js
@@ -1,9 +1,59 @@
-let navbar = document.querySelector('.header .navbar');
-
-document.querySelector('#menu-btn').onclick = () =>{
-    navbar.classList.add('active');
+function myFunction() {
+    var x = document.querySelector("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
 }
 
-document.querySelector('#close-navbar').onclick = () =>{
-    navbar.classList.remove('active');
+function redirectToGetStarted() {
+    // Redirect to the Get Started page
+    window.location.href = '../log in- signup page/signup.html';
 }
+function redirectToLogIn(){
+    window.location.href = '../log in- signup page/login.html'
+}
+function redirectToHistory() {
+    window.location.href = '../dropdowns/history.html';
+}
+
+function redirectToWhatWeDo() {
+    window.location.href = '../dropdowns/WWD.html';
+}
+
+function redirectToCreators() {
+    window.location.href = '../dropdowns/creators.html';
+}
+
+function redirectToContact() {
+    window.location.href = 'contact.html';
+}
+
+function redirectToFAQ() {
+    window.location.href = '../navbar-footer/faq.html';
+}
+
+function redirectToHelp() {
+    window.location.href = '../navbar-footer/help.html';
+}
+
+function redirectToTermsandConditions() {
+    window.location.href = '../navbar-footer/TAC.html';
+}
+
+function redirectToTermsandConditions() {
+    window.location.href = '../navbar-footer/TAC.html';
+}
+
+function redirectToPrivacyandPolicy() {
+    window.location.href = '../navbar-footer/PAP.html';
+}
+
+function redirectToHome() {
+    window.location.href = '../intro page/index.html';
+}
+
+
+
+
